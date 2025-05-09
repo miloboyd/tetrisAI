796d8a2f15f13552448808930a79dc69d2736f10
Milo Boyd <milocb123@gmail.com>
2025-05-10 00:58:29 +1000

    modified git repo with own changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# tetrisAI
-Designing a AI model to play an original and customised tetris game to achieve a high score.  
+# TetrisAI
+
+This GitHub repository defines an AI model created to play tetris. This repository is a modification of a base template below:
+
+https://github.com/educ8s/Python-Tetris-Game-Pygame.git
+
+
+
--- game.py
+++ game.py
@@ -1,218 +1,177 @@
-import pygame
+from grid import Grid
+from blocks import *
 import random
+import pygame
 
-colors = [
-    (0, 0, 0),
-    (120, 37, 179),
-    (100, 179, 179),
-    (80, 34, 22),
-    (80, 134, 22),
-    (180, 34, 22),
-    (180, 34, 122),
-]
+class Game:
+	def __init__(self):
+		self.grid = Grid()
+		self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+		self.current_block = self.get_random_block()
+		self.next_block = self.get_random_block()
+		self.game_over = False
+		self.score = 0
+		self.lock_delay = 500 # Lock delay in milliseconds 
+		self.lock_delay_timer = 0 # Timer to track current lock delay
+		self.locking_phase = False # Flag to indicate if piece is in locking phase
 
+	def update_score(self, lines_cleared, move_down_points):
+		if lines_cleared == 1:
+			self.score += 100
+		elif lines_cleared == 2:
+			self.score += 300
+		elif lines_cleared == 3:
+			self.score += 500
+		elif lines_cleared == 4:
+			self.score += 800
 
-class Figure:
-    x = 0
-    y = 0
+		self.score += move_down_points
 
-    figures = [
-        [[1, 5, 9, 13], [4, 5, 6, 7]],
-        [[4, 5, 9, 10], [2, 6, 5, 9]],
-        [[6, 7, 9, 10], [1, 5, 6, 10]],
-        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
-        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],
-        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
-        [[1, 2, 5, 6]],
-    ]
+	def get_random_block(self):
+		if len(self.blocks) == 0:
+			self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+		block = random.choice(self.blocks)
+		self.blocks.remove(block)
+		return block
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.type = random.randint(0, len(self.figures) - 1)
-        self.color = random.randint(1, len(colors) - 1)
-        self.rotation = 0
+	def move_left(self):
+		self.current_block.move(0, -1)
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.move(0, 1)
+		elif self.locking_phase:
+			self.lock_delay_timer = self.lock_delay
 
-    def image(self):
-        return self.figures[self.type][self.rotation]
+	def move_right(self):
+		self.current_block.move(0, 1)
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.move(0, -1)
+		elif self.locking_phase: 
+			self.lock_delay_timer = self.lock_delay
 
-    def rotate(self):
-        self.rotation = (self.rotation + 1) % len(self.figures[self.type])
+	def move_down(self):
+		self.current_block.move(1, 0)
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.move(-1, 0)
+			
+			if not self.locking_phase:
+				self.locking_phase = True
+				self.lock_delay_timer = self.lock_delay
+			else: 
+				self.locking_phase = False
+	
+	def hard_drop(self):
+		"""
+		Drops the current block all the way down and locks it in place.
+		"""
+		# Keep moving down until collision
+		drop_distance = 0
+		while True:
+			self.current_block.move(1, 0)
+			if self.block_inside() == False or self.block_fits() == False:
+				self.current_block.move(-1, 0)  # Move back up
+				break
+			drop_distance += 1
+		
+		# Add points for hard drop - typically 2 points per cell dropped
+		self.update_score(0, drop_distance * 2)
 
+		self.locking_phase = False
+		
+		# Lock the block in place
+		tiles = self.current_block.get_cell_positions()
+		for position in tiles:
+			self.grid.grid[position.row][position.column] = self.current_block.id
+		
+		# Check and clear full rows
+		rows_cleared = self.grid.clear_full_rows()
+		if rows_cleared > 0:
+			self.update_score(rows_cleared, 0)
+		
+		# Set up the next block
+		self.current_block = self.next_block
+		self.next_block = self.get_random_block()
+		
+		# Check if game is over
+		if self.block_fits() == False:
+			self.game_over = True
 
-class Tetris:
-    def __init__(self, height, width):
-        self.level = 2
-        self.score = 0
-        self.state = "start"
-        self.field = []
-        self.height = 0
-        self.width = 0
-        self.x = 100
-        self.y = 60
-        self.zoom = 20
-        self.figure = None
-    
-        self.height = height
-        self.width = width
-        self.field = []
-        self.score = 0
-        self.state = "start"
-        for i in range(height):
-            new_line = []
-            for j in range(width):
-                new_line.append(0)
-            self.field.append(new_line)
+	def lock_block(self):
+		tiles = self.current_block.get_cell_positions()
+		for position in tiles:
+			self.grid.grid[position.row][position.column] = self.current_block.id
+		self.current_block = self.next_block
+		self.next_block = self.get_random_block()
+		rows_cleared = self.grid.clear_full_rows()
+		if rows_cleared > 0:
+			self.update_score(rows_cleared, 0)
+		if self.block_fits() == False:
+			self.game_over = True
 
-    def new_figure(self):
-        self.figure = Figure(3, 0)
+	def reset(self):
+		self.grid.reset()
+		self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+		self.current_block = self.get_random_block()
+		self.next_block = self.get_random_block()
+		self.score = 0
 
-    def intersects(self):
-        intersection = False
-        for i in range(4):
-            for j in range(4):
-                if i * 4 + j in self.figure.image():
-                    if i + self.figure.y > self.height - 1 or \
-                            j + self.figure.x > self.width - 1 or \
-                            j + self.figure.x < 0 or \
-                            self.field[i + self.figure.y][j + self.figure.x] > 0:
-                        intersection = True
-        return intersection
+	def block_fits(self):
+		tiles = self.current_block.get_cell_positions()
+		for tile in tiles:
+			if self.grid.is_empty(tile.row, tile.column) == False:
+				return False
+		return True
 
-    def break_lines(self):
-        lines = 0
-        for i in range(1, self.height):
-            zeros = 0
-            for j in range(self.width):
-                if self.field[i][j] == 0:
-                    zeros += 1
-            if zeros == 0:
-                lines += 1
-                for i1 in range(i, 1, -1):
-                    for j in range(self.width):
-                        self.field[i1][j] = self.field[i1 - 1][j]
-        self.score += lines ** 2
+	'''
+	def rotate(self):
+		self.current_block.rotate()
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.undo_rotation()
+	'''
 
-    def go_space(self):
-        while not self.intersects():
-            self.figure.y += 1
-        self.figure.y -= 1
-        self.freeze()
+	def rotate_clockwise(self):
+		self.current_block.rotate_clockwise()
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.rotate_counterclockwise()
+		elif self.locking_phase:
+			self.lock_delay_timer = self.lock_delay
+	
+	def rotate_counterclockwise(self):
+		self.current_block.rotate_counterclockwise()
+		if self.block_inside() == False or self.block_fits() == False:
+			self.current_block.rotate_clockwise()
+		elif self.locking_phase:
+			self.lock_delay_timer = self.lock_delay
 
-    def go_down(self):
-        self.figure.y += 1
-        if self.intersects():
-            self.figure.y -= 1
-            self.freeze()
+	def block_inside(self):
+		tiles = self.current_block.get_cell_positions()
+		for tile in tiles:
+			if self.grid.is_inside(tile.row, tile.column) == False:
+				return False
+		return True
 
-    def freeze(self):
-        for i in range(4):
-            for j in range(4):
-                if i * 4 + j in self.figure.image():
-                    self.field[i + self.figure.y][j + self.figure.x] = self.figure.color
-        self.break_lines()
-        self.new_figure()
-        if self.intersects():
-            self.state = "gameover"
+	def draw(self, screen):
+		self.grid.draw(screen)
+		self.current_block.draw(screen, 11, 11)
 
-    def go_side(self, dx):
-        old_x = self.figure.x
-        self.figure.x += dx
-        if self.intersects():
-            self.figure.x = old_x
+		if self.next_block.id == 3:
+			self.next_block.draw(screen, 255, 290)
+		elif self.next_block.id == 4:
+			self.next_block.draw(screen, 255, 280)
+		else:
+			self.next_block.draw(screen, 270, 270)
 
-    def rotate(self):
-        old_rotation = self.figure.rotation
-        self.figure.rotate()
-        if self.intersects():
-            self.figure.rotation = old_rotation
-
-
-# Initialize the game engine
-pygame.init()
-
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GRAY = (128, 128, 128)
-
-size = (400, 500)
-screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("Tetris")
-
-# Loop until the user clicks the close button.
-done = False
-clock = pygame.time.Clock()
-fps = 25
-game = Tetris(20, 10)
-counter = 0
-
-pressing_down = False
-
-while not done:
-    if game.figure is None:
-        game.new_figure()
-    counter += 1
-    if counter > 100000:
-        counter = 0
-
-    if counter % (fps // game.level // 2) == 0 or pressing_down:
-        if game.state == "start":
-            game.go_down()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                game.rotate()
-            if event.key == pygame.K_DOWN:
-                pressing_down = True
-            if event.key == pygame.K_LEFT:
-                game.go_side(-1)
-            if event.key == pygame.K_RIGHT:
-                game.go_side(1)
-            if event.key == pygame.K_SPACE:
-                game.go_space()
-            if event.key == pygame.K_ESCAPE:
-                game.__init__(20, 10)
-
-    if event.type == pygame.KEYUP:
-            if event.key == pygame.K_DOWN:
-                pressing_down = False
-
-    screen.fill(WHITE)
-
-    for i in range(game.height):
-        for j in range(game.width):
-            pygame.draw.rect(screen, GRAY, [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
-            if game.field[i][j] > 0:
-                pygame.draw.rect(screen, colors[game.field[i][j]],
-                                 [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
-
-    if game.figure is not None:
-        for i in range(4):
-            for j in range(4):
-                p = i * 4 + j
-                if p in game.figure.image():
-                    pygame.draw.rect(screen, colors[game.figure.color],
-                                     [game.x + game.zoom * (j + game.figure.x) + 1,
-                                      game.y + game.zoom * (i + game.figure.y) + 1,
-                                      game.zoom - 2, game.zoom - 2])
-
-    font = pygame.font.SysFont('Calibri', 25, True, False)
-    font1 = pygame.font.SysFont('Calibri', 65, True, False)
-    text = font.render("Score: " + str(game.score), True, BLACK)
-    text_game_over = font1.render("Game Over", True, (255, 125, 0))
-    text_game_over1 = font1.render("Press ESC", True, (255, 215, 0))
-
-    screen.blit(text, [0, 0])
-    if game.state == "gameover":
-        screen.blit(text_game_over, [20, 200])
-        screen.blit(text_game_over1, [25, 265])
-
-    pygame.display.flip()
-    clock.tick(fps)
-
-pygame.quit()
+	def update(self, delta_time):
+		"""
+		Update game state based on time elapsed.
+		
+		Args:
+			delta_time: Time elapsed since last update in milliseconds
+		"""
+		# Only update lock delay timer if in locking phase
+		if self.locking_phase:
+			self.lock_delay_timer -= delta_time
+			
+			# If timer expired, lock the block
+			if self.lock_delay_timer <= 0:
+				self.lock_block()
+				self.locking_phase = False
